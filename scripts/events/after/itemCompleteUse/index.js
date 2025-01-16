a4753c25324f139a3a92e2b19334a14a20b61ed9
RatItBe <re228424@gmail.com>
2025-01-16 23:20:36 +0900

    출혈소생템 임시 추가

--- scripts/events/after/itemCompleteUse/index.js
+++ scripts/events/after/itemCompleteUse/index.js
@@ -1,5 +1,6 @@
 import { EquipmentSlot, ItemStack } from "@minecraft/server";
 import { releaseWeaponList } from 'data/gun';
+import { BleedSystem } from "class/bleedSystem";
 
 export function itemCompleteUse(eventData) {
     const item = eventData.itemStack; // 사용된 아이템을 item 변수에 저장
@@ -8,6 +9,11 @@ export function itemCompleteUse(eventData) {
     const weapon = releaseWeaponList.find(w => w.emptyWeaponName === item.typeId);
     if (weapon) {
         reloadGun(player, weapon)
+    }
+    else if (item.typeId.includes("revive")) {
+        if ((player.getDynamicProperty("bleed") === true) && (player.getDynamicProperty("reviveCooldown") < 1)) {
+            BleedSystem.selfRevive(player, item.typeId);
+        }
     }
 }
 
