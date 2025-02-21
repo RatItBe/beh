6411046d505f1ef40da811663ec60f670df0244a
FRIEDsoboro <pkmqww@google.com>
2025-02-21 20:05:35 +0900

    레시피 추가 및 기타 이름 변경 및 기타등등등

--- scripts/data/food.js
+++ scripts/data/food.js
@@ -9,9 +9,47 @@ export const food = [
         effects: [
             {
                 id: "saturation",
-                amplifier: 1,
-                duration: 1,
+                amplifier: 0,
+                duration: 1200,
+                showParticles: true
+            },
+            {
+                id: "regeneration",
+                amplifier: 0,
+                duration: 1200,
                 showParticles: false
+            }
+        ]
+    },
+    {
+        foodID: "fs:fried_fish",
+        eatNoise: {
+            id: "random.burp",
+            volume: 1,
+            pitch: 0.95
+        },
+        effects: [
+            {
+                id: "saturation",
+                amplifier: 0,
+                duration: 1,
+                showParticles: true
+            }
+        ]
+    },
+    {
+        foodID: "fs:big_dumpling",
+        eatNoise: {
+            id: "random.burp",
+            volume: 1,
+            pitch: 0.95
+        },
+        effects: [
+            {
+                id: "saturation",
+                amplifier: 0,
+                duration: 1,
+                showParticles: true
             }
         ]
     },
--- scripts/data/potRecipe.js
+++ scripts/data/potRecipe.js
@@ -1,10 +1,5 @@
 export const potRecipe2 = [
     {
-        recipeName: "minecraft:cooked_beef",
-        ingredient1: "minecraft:beef",
-        ingredient2: "minecraft:rotten_flesh"
-    },
-    {
         recipeName: "fs:fried_potato",
         ingredient1: "fs:cooking_oil",
         ingredient2: "minecraft:potato"
@@ -18,30 +13,40 @@ export const potRecipe2 = [
         recipeName: "fs:fried_fish",
         ingredient1: "fs:cooking_oil",
         ingredient2: "fs:cut_cod"
+    },
+    {
+        recipeName: "fs:fried_chicken",
+        ingredient1: "fs:coooking_oil",
+        ingredient2: "fs:cut_chicken"
+    },
+    {
+        recipeName: "fs:hotdog",
+        ingredient1: "fs:cut_bread",
+        ingredient2: "fs:cut_pork"
     }
 ]
 
 export const potRecipe3 = [
     {
-        recipeName: "minecraft:cooked_beef",
-        ingredient1: "minecraft:beef",
-        ingredient2: "minecraft:beef",
-        ingredient3: "minecraft:rotten_flesh"
+        recipeName: "fs:big_dumpling",
+        ingredient1: "fs:dough",
+        ingredient2: "fs:cut_pork",
+        ingredient3: "minecraft:carrot"
     },
     {
         recipeName: "fs:bacon_egg",
         ingredient1: "fs:bacon",
-        ingredient2: "fs:bacon",
+        ingredient2: "fs:cut_bread",
         ingredient3: "fs:fried_egg"
     }
 ]
 
 export const potRecipe4 = [
     {
-        recipeName: "minecraft:cooked_beef",
-        ingredient1: "minecraft:beef",
-        ingredient2: "minecraft:beef",
-        ingredient3: "minecraft:rotten_flesh",
-        ingredient4: "minecraft:rotten_flesh"
+        recipeName: "fs:sushi_platter",
+        ingredient1: "fs:cut_salmon",
+        ingredient2: "fs:rice",
+        ingredient3: "minecraft:kelp",
+        ingredient1: "fs:cut_salmon"
     }
 ]
