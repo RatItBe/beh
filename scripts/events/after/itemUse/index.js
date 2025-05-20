bba8f51593934c5ca812b8ae109e4bdcb5cfe12b
RatItBe <re228424@gmail.com>
2025-05-20 20:50:32 +0900

    구조물 복사 기능 폐기

--- scripts/events/after/itemUse/index.js
+++ scripts/events/after/itemUse/index.js
@@ -1,7 +1,6 @@
 import { system, EquipmentSlot } from "@minecraft/server";
 import { DebugSystem } from "class/debugSystem";
 import { RangedWeaponSystem } from "class/weapon/rangedWeaponSystem";
-import { structureSystem } from "class/structureSystem";
 import { useWeapon } from "data/rangedWeapon";
 
 export function itemUse(eventData) {
@@ -10,19 +9,6 @@ export function itemUse(eventData) {
     
     const equippable = player.getComponent("minecraft:equippable");
     const mainhand = equippable.getEquipmentSlot(EquipmentSlot.Mainhand);
-
-    if (item.typeId === "minecraft:compass") {
-        if (player.isSneaking) {
-            player.setDynamicProperty("x", undefined);
-            player.setDynamicProperty("y", undefined);
-            player.setDynamicProperty("z", undefined);
-            player.sendMessage("좌표가 초기화되었습니다.");
-        }
-        else {
-            const block = player.getBlockFromViewDirection().block;
-            structureSystem.saveStructure(player, block);
-        }
-    }
 
     const weapon = useWeapon.find(w => w.weaponName === item.typeId);
     if (weapon) {
--- scripts/events/before/chatSend/index.js
+++ scripts/events/before/chatSend/index.js
@@ -1,5 +1,4 @@
 import { DailyRewardSystem } from "class/dailyRewardSystem";
-import { structureSystem } from "class/structureSystem";
 
 export function chatSend(eventData) {
     const player = eventData.sender;
@@ -8,9 +7,5 @@ export function chatSend(eventData) {
     if (message === "!출석") { // 출석 체크 명령어
         eventData.cancel = true;
         DailyRewardSystem.rewardReceipt(player);
-    }
-    else if (message === "!구조물") { // 클립보드로 구조물을 복원하는 명령어
-        eventData.cancel = true;
-        structureSystem.loadstructure(player);
     }
 }
