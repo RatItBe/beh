e4e46d5465dcaaa8072904346b18bb1bf6460576
RatItBe <re228424@gmail.com>
2025-07-11 20:44:20 +0900

    스테이터스 표시 빈도 수정

--- scripts/events/runInterval/index2.js
+++ scripts/events/runInterval/index2.js
@@ -2,5 +2,71 @@ import { MeleeWeaponSystem } from "class/weapon/meleeWeaponSystem";
 
 // 0.1초에 한 번 실행될 코드
 export function runInterval2(player) {
+    player.runCommand(`titleraw @s actionbar {
+    "rawtext": [
+        {
+            "text": "§a스태미나 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "stamina"
+            }
+        },
+        {
+            "text": " / §b마나 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "mana"
+            }
+        },
+        {
+            "text": " / §6탄알 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "bullet"
+            }
+        }
+    ]
+}`);
+
     MeleeWeaponSystem.cooldownCheck(player);
 }
+
+
+/* 스테이터스 양식
+{
+    "rawtext": [
+        {
+            "text": "§a스태미나 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "stamina"
+            }
+        },
+        {
+            "text": " / §b마나 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "mana"
+            }
+        },
+        {
+            "text": " / §6탄알 : "
+        },
+        {
+            "score": {
+                "name": "*",
+                "objective": "bullet"
+            }
+        }
+    ]
+}*/
